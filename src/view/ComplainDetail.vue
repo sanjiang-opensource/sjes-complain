e6749ba1970ce22d1ad2524496eed301b0f464f3
zhangyunan <1196318925@qq.com>
2017-05-23 20:26:33 +0800

    complain

---
+++ src/view/ComplainDetail.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <panel li></panel>
+    </div>
+</template>
+
+<script>
+import { Group, Cell, Scroller, Panel} from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Scroller,
+    Panel
+  },
+  data () {
+    return {
+      type: '1',
+      list: [{
+        title: '标题一',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+      }, {
+        title: '标题二',
+        desc: '由各种物质组成的巨型球状天体，叫做星球。星球有一定的形状，有自己的运行轨道。',
+      }],
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/view/ComplainList.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <scroller v-for="">
+
+       <cell title="啦啦啦">
+
+       </cell>
+
+    </scroller>
+  </div>
+</template>
+
+<script>
+import { Group, Cell, Scroller, Panel} from 'vux'
+
+export default {
+  components: {
+    Group,
+    Cell,
+    Scroller,
+    Panel
+  },
+  data () {
+    return {
+      header:"",
+      list: [{
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题一',
+        desc: '由各种。',
+        url: '/component/cell'
+      }, {
+        src: 'http://placeholder.qiniudn.com/60x60/3cc51f/ffffff',
+        title: '标题二',
+        desc: '由各种',
+        url: {
+          path: '/component/radio',
+          replace: false
+        }
+      }],
+      footer:"查看更多",
+      items:[
+        {messgae:""}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+.vux-demo {
+  text-align: center;
+}
+.logo {
+  width: 100px;
+  height: 100px
+}
+</style>
