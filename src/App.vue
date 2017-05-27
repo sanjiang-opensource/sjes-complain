d2ec2979392852f5a9228b0e7c31bf764ed3638a
fudongwei <fudongwei@sanjiang365.com>
2017-05-27 09:50:29 +0800

    实现列表分页

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,10 @@
 <style lang="less">
   @import '~vux/src/styles/reset.less';
 
-  html, body {
-    height: 100%;
-    width: 100%;
-    overflow-x: hidden;
-  }
-
   body {
     background-color: #fbf9fe;
   }
+</style>
+<style lang="scss">
+  @import "assets/list";
 </style>
