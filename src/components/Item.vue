33c88f9c0bbd38a0e27102b3a5f7395441694106
zhangyunan <1196318925@qq.com>
2017-07-12 11:56:22 +0800

    修改

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -1,9 +1,10 @@
 <template>
-  <div @click="onClick">
+  <div>
     <cell title="投诉时间 ：" :value="item.complainTime | timeToString" value-align="left" label-margin-left="2em"></cell>
     <cell title="受理部门 ：" :value="item.receiveDept" value-align="left" label-margin-left="2em"></cell>
     <cell title="投诉内容 ：" :inline-desc="item.complainContent" style="word-break: break-all "></cell>
-    <cell :border-intent="false" :title="item.overTime | changeTitle" :style="changeStyle">
+    <cell :border-intent="false" :title="item.overTime | changeTitle" :value="item.complainStat | statusName"
+          style="height: 60px;color: red;" :link="'/complain/detail/'+item.id+'/'+workerId">
       <img slot="icon" width="25" class="img" src="../assets/outtime.png">
       <div slot="value">
         <span :style="changeValueStyle">{{item.complainStat | statusName}}</span>
